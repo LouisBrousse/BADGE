35d905a3139bf090f29f2e7d7286ebad82dbb90c
aymericbarbot <aymeric.barbot@neusta.fr>
2025-03-05 14:49:11 +0100

    src mis a jour du prof

--- src/controlleur_acces.py
+++ src/controlleur_acces.py
@@ -1,2 +1,12 @@
-class ControlleurAcces:
-    pass
+from lecteur import Lecteur
+from porte import Porte
+
+
+class ControleurAcces:
+    def __init__(self, porte: Porte, lecteur: Lecteur):
+        self.__lecteur = lecteur
+        self.__porte = porte
+
+    def interroger_lecteur(self):
+        if self.__lecteur.poll() is not None:
+            self.__porte.demander_ouverture()
