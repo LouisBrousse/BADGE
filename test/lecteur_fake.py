556dcab4c6517b0dbf63bd7f5e576953308e7ec2
aymericbarbot <aymeric.barbot@neusta.fr>
2025-03-05 15:31:48 +0100

    premier test ok

--- test/lecteur_fake.py
+++ test/lecteur_fake.py
@@ -1,3 +1,14 @@
-class Lecteur:
+from lecteur import Lecteur
+
+
+class Lecteurfake(Lecteur):
+    def __init__(self):
+        self.__numero_badge_detecte = None
+
+    def poll(self) -> int | None:
+        numero_detecte = self.__numero_badge_detecte
+        self.__numero_badge_detecte = None
+        return numero_detecte
+
     def simuler_presentation_badge(self):
-        pass
+        self.__numero_badge_detecte = 0
--- test/porte_spy.py
+++ test/porte_spy.py
@@ -1,2 +1,9 @@
-class PorteSpy:
-    pass
+from porte import Porte
+
+
+class PorteSpy(Porte):
+    def __init__(self):
+        self.signal_ouverture_reçu = False
+
+    def demander_ouverture(self):
+        self.signal_ouverture_reçu = True
